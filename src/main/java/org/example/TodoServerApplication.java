f945e5e818f3ac54f87b41aa8b4e0859444aa8f5
qudtjs0753 <qudtjs0753@naver.com>
2021-10-10 01:25:42 +0900

    Feat: TodoController 생성

--- src/main/java/org/example/TodoServerApplication.java
+++ src/main/java/org/example/TodoServerApplication.java
@@ -1,8 +1,11 @@
 package org.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class TodoServerApplication {
     public static void main(String[] args) {
-        System.out.println("hello Todo");
-
+        SpringApplication.run(TodoServerApplication.class, args);
     }
 }
